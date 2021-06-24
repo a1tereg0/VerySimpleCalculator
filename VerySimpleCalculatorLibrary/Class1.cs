d122697882080d2057532a702217aeb5c58a7c58
Prashant Dahal <prashantdah41@gmail.com>
2021-06-23 23:14:44 -0400

    Create a class library project and implement calculator functions

---
+++ VerySimpleCalculatorLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VerySimpleCalculatorLibrary
+{
+    public static class SimpleCalculator
+    {
+        static public double add(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+
+        static public double sub(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        static public double mul(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        static public double div(double value1, double value2)
+        {
+            if (value2 == 0.0)
+                throw new DivideByZeroException("Cannot divide by Zero");
+            return value1 / value2;
+            
+        }
+    }
+}
