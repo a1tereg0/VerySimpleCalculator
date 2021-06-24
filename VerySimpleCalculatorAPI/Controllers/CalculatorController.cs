a84b62f49a44c7a27d64800e885eee7fd42fbde5
Prashant Dahal <prashantdah41@gmail.com>
2021-06-23 23:43:46 -0400

    Create .NET Core Web API Project and add relevant api endpoint functions using the library

---
+++ VerySimpleCalculatorAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VerySimpleCalculatorLibrary;
+
+namespace VerySimpleCalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public double add(double value1, double value2)
+        {
+            return SimpleCalculator.add(value1, value2);
+        }
+
+        [HttpGet]
+        public double sub(double value1, double value2)
+        {
+            return SimpleCalculator.add(value1, value2);
+        }
+
+        [HttpGet]
+        public double mul(double value1, double value2)
+        {
+            return SimpleCalculator.add(value1, value2);
+        }
+
+        [HttpGet]
+        public double div(double value1, double value2)
+        {
+            return SimpleCalculator.add(value1, value2);
+        }
+    }
+}
