53cde8922c045d998b3522d0b1fbb9cc682d0ee5
Prashant Dahal <prashantdah41@gmail.com>
2021-06-24 20:52:09 -0400

    Add some test methods for calcutator operations

--- VerySimpleCalculatorTests/UnitTest1.cs
+++ VerySimpleCalculatorTests/UnitTest1.cs
@@ -1,13 +1,74 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerySimpleCalculatorLibrary;
 
 namespace VerySimpleCalculatorTests
 {
     [TestClass]
+
+
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataRow(1, 3)]
+        [DataRow(0, 1)]
+        [DataRow(6, 9)]
+        // Commutative Property: a + b = b + a also, a * b = b * a
+        public void AdditionAndMultiplicationIsCommutative(double a, double b)
         {
+            double aPlusB = SimpleCalculator.add(a, b);
+            double bPlusA = SimpleCalculator.add(b, a);
+            double aIntoB = SimpleCalculator.mul(a, b);
+            double bIntoA = SimpleCalculator.mul(b, a);
+            Assert.AreEqual(aPlusB, bPlusA);
+            Assert.AreEqual(aIntoB, bIntoA);
         }
+        
+        [TestMethod]
+        [DataRow(5,0)]
+        [DataRow(0,9)]
+        // Addititive Identity Property: a + 0 = 0 + a = 0
+        public void AdditiveIdentity(double a, double b)
+        {
+            if (a == 0)
+            {
+                Assert.AreEqual(b, SimpleCalculator.add(a, b));
+            } else if (b == 0)
+            {
+                Assert.AreEqual(a, SimpleCalculator.add(a, b));
+            }
+        }
+
+        [TestMethod]
+        [DataRow(3,6)]
+        [DataRow(5,5)]
+        [DataRow(56, 11)]
+        // a - b is not equal to b - a, unless both values are the same.
+        // a / b is not equal to b / a, unless both values are the same.
+        public void SubstractionAndDivisionIsNotCommutative(double a, double b)
+        {
+            if (a != b)
+            {
+                Assert.AreEqual(SimpleCalculator.sub(a, b), SimpleCalculator.sub(b, a));
+                Assert.AreEqual(SimpleCalculator.div(a, b), SimpleCalculator.div(b, a));
+            }
+        }
+
+        [TestMethod]
+        [DataRow(1, 5)]
+        [DataRow(45, 1)]
+        // Multiplicative Identity Property: a * 1 = 1 * a = a
+        public void MultiplicativeIdentity(double a, double b)
+        {
+            if (a == 1)
+            {
+                Assert.AreEqual(b, SimpleCalculator.mul(a, b));
+            }
+            else if (b == 1)
+            {
+                Assert.AreEqual(a, SimpleCalculator.mul(a, b));
+            }
+        }
+
+
     }
 }
